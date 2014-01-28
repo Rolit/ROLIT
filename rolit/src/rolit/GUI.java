a2943271a9fb2bfc2ee9bfc51a6e4dab06c501f6
Josje1993 <josje-vt-padje@hotmail.com>
2014-01-28 19:46:06 +0100

    added a startFrame

--- rolit/src/rolit/GUI.java
+++ rolit/src/rolit/GUI.java
@@ -1,78 +1,153 @@
 package rolit;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class GUI extends JFrame implements Observer{
+@SuppressWarnings("serial")
+public class GUI extends JFrame implements Observer, ActionListener{
 	
 	// -- Instance variables -----------------------------------------
 
 	private JLabel  lb;
 	public JLabel validMoveLabel;
+	
 	private JButton[] buttons;
 	private JButton replay;
+	private JButton startButton;
+	private JComboBox<String> numberChooser;
+	Game game = new Game();
+	private JFrame startFrame;
 	
 	// -- Constructors -----------------------------------------------
 	
 	public GUI(Game game) {
 	    super("GUI");
-	    init(game);
+	    startFrame();
 	}
 	
 	// -- Commands ---------------------------------------------------
 	
-	private void init(Game game) {
-		 Container c = getContentPane();
-		 c.setLayout(new GridLayout(0,8));
-		 buttons = new JButton[64]; 
-		 for(int x = 0; x < Board.DIM; x++){
-			 for(int y = 0; y < Board.DIM; y++){
-				int ind = Board.index(x, y);
-				buttons[ind] = new JButton();
-				if(x == 3 && y == 3){
-                    buttons[ind].setBackground(Color.RED);
-                    buttons[ind].setEnabled(false);
-				}
-				else if(x == 4 && y == 3){
-                    buttons[ind].setBackground(Color.BLUE);
-                    buttons[ind].setEnabled(false);
-				}
-				else if(x == 3 && y == 4){
-                    buttons[ind].setBackground(Color.YELLOW);
-                    buttons[ind].setEnabled(false);
-				}
-				else if(x == 4 && y == 4){
-                    buttons[ind].setBackground(Color.GREEN);
-                    buttons[ind].setEnabled(false);
-				}
-				else{
-                    buttons[ind].setBackground(Color.BLACK);
-				}
-				c.add(buttons[ind]);
-			}
-		}
-
-		lb = new JLabel("It is ROOD's turn");
-		validMoveLabel = new JLabel("");
+	public void init(Game game) {
 		
+		rolitFrame();
 		new RolitController(buttons, game, replay);
-		
-		c.add(lb);
-		c.add(validMoveLabel);
-		setSize(800,900); 
-		setVisible(true);
-		
-		replay = new JButton("New Game?");
-		replay.setEnabled(false);
-		c.add(replay);
-		
 		game.addObserver(this);
 	}
 	
+	public void startFrame(){
+		startFrame = new JFrame();
+		startFrame.setLayout(new BorderLayout());
+		startFrame.add(numberPlayers(), BorderLayout.NORTH);
+		startFrame.add(startPanel(), BorderLayout.SOUTH);
+		startFrame.setSize(400,150);
+		startFrame.setVisible(true);
+		
+	}
+	
+	public JPanel startPanel(){
+		startButton = new JButton("Start Game");
+		JPanel startPanel = new JPanel();
+		startPanel.add(startButton);
+		startButton.addActionListener(this);
+		
+		return startPanel;
+	}
+	
+	public JPanel numberPlayers(){
+		numberChooser = new JComboBox<String>();
+		
+		numberChooser.addItem("2 spelers");
+		numberChooser.addItem("3 spelers");
+		numberChooser.addItem("4 spelers");
+		
+		numberChooser.addActionListener(this);
+		JPanel numberPlayers = new JPanel();
+		numberPlayers.add(numberChooser);
+		numberPlayers.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black), "Choose number of Players"));
+		return numberPlayers;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		if(numberChooser.getSelectedItem() == "2 spelers"){
+			Game.NUMBER_PLAYERS = 2;
+		}
+		else if(numberChooser.getSelectedItem() == "3 spelers"){
+			Game.NUMBER_PLAYERS = 3;
+		}
+		else if(numberChooser.getSelectedItem() == "4 spelers"){
+			Game.NUMBER_PLAYERS = 4;
+		}
+		if(e.getSource() == startButton){
+			init(game);
+			startFrame.dispose();
+		}
+	}
+	
+	public void rolitFrame(){
+		JFrame rolitFrame = new JFrame();
+		rolitFrame.setLayout(new BorderLayout());
+		rolitFrame.add(rolitPanel(), BorderLayout.CENTER);
+		rolitFrame.add(commentPanel(), BorderLayout.EAST);
+		
+		rolitFrame.setSize(1000,900); 
+		rolitFrame.setVisible(true);
+	}
+	
+	private JPanel commentPanel(){
+		JPanel commentPanel = new JPanel(); 
+		commentPanel.setLayout(new GridLayout(0,1));
+		
+		lb = new JLabel("It is ROOD's turn");
+		validMoveLabel = new JLabel("");
+		
+		commentPanel.add(lb);
+		commentPanel.add(validMoveLabel);
+		
+		
+		replay = new JButton("New Game?");
+		replay.setEnabled(false);
+		commentPanel.add(replay);
+		return commentPanel;
+	}
+
+	private JPanel rolitPanel(){
+		JPanel rolitPanel = new JPanel();
+		rolitPanel.setLayout(new GridLayout(0,8));
+		buttons = new JButton[64]; 
+		for(int x = 0; x < Board.DIM; x++){
+			for(int y = 0; y < Board.DIM; y++){
+				int ind = Board.index(x, y);
+				buttons[ind] = new JButton();
+				if(x == 3 && y == 3){
+					buttons[ind].setBackground(Color.RED);
+					buttons[ind].setEnabled(false);
+				}
+				else if(x == 4 && y == 3){
+					buttons[ind].setBackground(Color.BLUE);
+					buttons[ind].setEnabled(false);
+				}
+				else if(x == 3 && y == 4){
+					buttons[ind].setBackground(Color.YELLOW);
+					buttons[ind].setEnabled(false);
+				}
+				else if(x == 4 && y == 4){
+                   buttons[ind].setBackground(Color.GREEN);
+                   buttons[ind].setEnabled(false);
+				}
+				else{
+                   buttons[ind].setBackground(Color.BLACK);
+				}
+				rolitPanel.add(buttons[ind]);
+			}
+		}
+		return rolitPanel;
+	}
 	
 	
 	public void update(Observable o, Object arg) {
@@ -96,7 +171,7 @@ public class GUI extends JFrame implements Observer{
 			}
 		}
 		else{
-			lb.setText("It's " + board.toString(mark) + "turn");
+			lb.setText("It's " + board.toString(mark) + "'s turn");
 			for (int x = 0; x < Board.DIM; x++){
 				for(int y = 0; y < Board.DIM; y++){
 					Color k = ((AbstractButton) buttons[Board.index(x, y)]).getBackground();
@@ -110,7 +185,6 @@ public class GUI extends JFrame implements Observer{
 				}
 			}
 		}
-		
 	}
 	
 	static public void main(String[] args) {
