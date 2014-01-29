0a7481e8df4248902e6546c6f3ead4240d920f00
Josje1993 <josje-vt-padje@hotmail.com>
2014-01-29 14:02:12 +0100

    gezorgd dat 2, 3, 4 mensen kunnen spelen

--- rolit/src/rolit/Rolit.java
+++ rolit/src/rolit/Rolit.java
@@ -11,17 +11,31 @@ public class Rolit {
 		}
 	}
 	
+	@SuppressWarnings("unused")
 	public static void main(String[] args){
 		if (args.length == Game.NUMBER_PLAYERS){
 			System.out.println("Computer Rolit");
 			System.out.println("--------------");
-			Player s1 = createPlayer(args[0], Mark1.ROOD);
-			Player s2 = createPlayer(args[1], Mark1.GROEN);
-			Game spel = new Game(s1, s2);
+			if (2 == Game.NUMBER_PLAYERS){
+				Player s1 = createPlayer(args[0], Mark1.ROOD);
+				Player s2 = createPlayer(args[1], Mark1.GROEN);
+			}
+			if (3 == Game.NUMBER_PLAYERS){
+				Player s1 = createPlayer(args[0], Mark1.ROOD);
+				Player s2 = createPlayer(args[1], Mark1.GEEL);
+				Player s3 = createPlayer(args[2], Mark1.GROEN);
+			}
+			if (4 == Game.NUMBER_PLAYERS){
+				Player s1 = createPlayer(args[0], Mark1.ROOD);
+				Player s2 = createPlayer(args[1], Mark1.GEEL);
+				Player s3 = createPlayer(args[2], Mark1.GROEN);
+				Player s4 = createPlayer(args[3], Mark1.BLAUW);
+			}
+			Game spel = new Game();
 			spel.start();
 		}
 		else{
-			System.out.println("usage: TicTacToe <name1> <nam2>");
+			System.out.println("usage: Rolit <name1> <name2> <name3> <name4>");
 		}
 	}
 }
