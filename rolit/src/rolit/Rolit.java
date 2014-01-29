194ae086147743580f456d3f98821f8b553ccd6c
Josje1993 <josje-vt-padje@hotmail.com>
2014-01-29 14:03:39 +0100

    naam spelers aangepast

--- rolit/src/rolit/Rolit.java
+++ rolit/src/rolit/Rolit.java
@@ -17,19 +17,19 @@ public class Rolit {
 			System.out.println("Computer Rolit");
 			System.out.println("--------------");
 			if (2 == Game.NUMBER_PLAYERS){
-				Player s1 = createPlayer(args[0], Mark1.ROOD);
-				Player s2 = createPlayer(args[1], Mark1.GROEN);
+				Player s0 = createPlayer(args[0], Mark1.ROOD);
+				Player s1 = createPlayer(args[1], Mark1.GROEN);
 			}
 			if (3 == Game.NUMBER_PLAYERS){
-				Player s1 = createPlayer(args[0], Mark1.ROOD);
-				Player s2 = createPlayer(args[1], Mark1.GEEL);
-				Player s3 = createPlayer(args[2], Mark1.GROEN);
+				Player s0 = createPlayer(args[0], Mark1.ROOD);
+				Player s1 = createPlayer(args[1], Mark1.GEEL);
+				Player s2 = createPlayer(args[2], Mark1.GROEN);
 			}
 			if (4 == Game.NUMBER_PLAYERS){
-				Player s1 = createPlayer(args[0], Mark1.ROOD);
-				Player s2 = createPlayer(args[1], Mark1.GEEL);
-				Player s3 = createPlayer(args[2], Mark1.GROEN);
-				Player s4 = createPlayer(args[3], Mark1.BLAUW);
+				Player s0 = createPlayer(args[0], Mark1.ROOD);
+				Player s1 = createPlayer(args[1], Mark1.GEEL);
+				Player s2 = createPlayer(args[2], Mark1.GROEN);
+				Player s3 = createPlayer(args[3], Mark1.BLAUW);
 			}
 			Game spel = new Game();
 			spel.start();
